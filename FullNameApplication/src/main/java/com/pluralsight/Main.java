c8cf3d560c514a56c16ede6a68589a3941b4dac7
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-16 10:42:33 -0400

    FullNameApplication/

--- FullNameApplication/src/main/java/com/pluralsight/Main.java
+++ FullNameApplication/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,42 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner keystrokes = new Scanner(System.in);
+
+        System.out.println("give me your name or else? :");
+
+        System.out.println("First name :");
+        String first = keystrokes.nextLine();
+
+        System.out.println("middle initial (press Enter to skip)? :");
+        String middle = keystrokes.nextLine();
+
+        System.out.println("last name? :");
+        String last = keystrokes.nextLine();
+
+        System.out.print("Enter your suffix (e.g., Jr, PhD) (press Enter to skip): ");
+        String suffix = keystrokes.nextLine().trim();
+
+
+        StringBuilder fullName = new StringBuilder();
+
+        if (middle.isEmpty()){
+            fullName.
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
