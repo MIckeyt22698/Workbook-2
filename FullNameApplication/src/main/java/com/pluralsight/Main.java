e80fdb9754cdb058a886d2273f5d559aec173aee
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-16 12:00:11 -0400

    FullNameApplication/

--- FullNameApplication/src/main/java/com/pluralsight/Main.java
+++ FullNameApplication/src/main/java/com/pluralsight/Main.java
@@ -6,16 +6,16 @@ public class Main {
     public static void main(String[] args){
         Scanner keystrokes = new Scanner(System.in);
 
-        System.out.println("give me your name or else? :");
+        System.out.println("give me your name or suffer");
 
         System.out.println("First name :");
-        String first = keystrokes.nextLine();
+        String first = keystrokes.nextLine().trim();
 
         System.out.println("middle initial (press Enter to skip)? :");
-        String middle = keystrokes.nextLine();
+        String middle = keystrokes.nextLine().trim();
 
         System.out.println("last name? :");
-        String last = keystrokes.nextLine();
+        String last = keystrokes.nextLine().trim();
 
         System.out.print("Enter your suffix (e.g., Jr, PhD) (press Enter to skip): ");
         String suffix = keystrokes.nextLine().trim();
