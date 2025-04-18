bf6a6f1561740a34cd8ccb057fcd941f9926882d
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-18 14:19:34 -0400

    LoopingExercises/

--- LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
+++ LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -3,10 +3,10 @@ package com.pluralsight;
 public class ForLoop {
 public static void main(String[] args){
 
-    for (int count = 1;count<= 5;count++){
-        System.out.println("I Love Java!");
+    for (int count = 10;count>= 1;count--){
+        System.out.println(count);
     }
-            
+
 
 
 
