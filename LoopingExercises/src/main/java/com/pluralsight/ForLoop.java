e655110ca7df3783b2a517b1ffdb730ac937c34e
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-18 14:42:00 -0400

    LoopingExercises/

--- LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
+++ LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -2,12 +2,13 @@ package com.pluralsight;
 
 public class ForLoop {
 public static void main(String[] args) throws InterruptedException {
+    System.out.println("This is Ground Control to bridge, count down in");
 
     for (int count = 10;count>= 1;count--){
         System.out.println(count);
         Thread.sleep(1000);
     }
-    System.out.println("Launch!");
+    System.out.println("We Have Liftoff!");
 
 
 
