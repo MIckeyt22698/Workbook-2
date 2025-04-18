9b33928502f19c9d624b4157936e3df8b82e267a
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-18 16:07:49 -0400

    RollTheDice/

--- RollTheDice/src/main/java/com/pluralsight/Main.java
+++ RollTheDice/src/main/java/com/pluralsight/Main.java
@@ -15,5 +15,17 @@ public class Main {
             System.out.println("Roll " + i + ":" +
                     " " + roll1 + " - " + roll2 + " Sum: " + sum);
 
+            if (sum == 2) {
+                count2++;
+            } else if (sum == 4) {
+                count4++;
+            } else if (sum == 6) {
+                count6++;
+            } else if (sum == 7) {
+                count7++;
+            }
+
         }
-    }}
+        System.out.println(count2 + " Twos " +  count4 + " Fours " + count6 + " Sixes " +count7 + " Sevens");
+    }
+}
