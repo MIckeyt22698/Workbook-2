1cbd630699416b247ad09f4e870a4362e7dbdeb1
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-21 11:44:08 -0400

    CustomerAplication/

---
+++ CustomerAplication/src/main/java/com/pluralsight/Customer.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+public class Customer {
+
+    private int id;
+    private String name;
+    private float outStandingBalance;
+
+
+    public Customer(int id, String name, float outStandingBalance) {
+        this.id = id;
+        this.name = name;
+        this.outStandingBalance = outStandingBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getOutStandingBalance() {
+        return outStandingBalance;
+    }
+
+    public void setOutStandingBalance(float outStandingBalance) {
+        this.outStandingBalance = outStandingBalance;
+    }
+}
---
+++ CustomerAplication/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args){
+        Customer customer = new Customer(100, "Mickey Teke", 100000);
+        System.out.println(customer);
+
+}
+}
