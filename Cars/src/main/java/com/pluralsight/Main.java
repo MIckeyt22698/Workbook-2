b9c5fae3b84774520e8a5d6b99d922cba48d655b
Melkias_Tekeste <mickeyt22698@gmail.com>
2025-04-21 14:27:32 -0400

    Cars/

--- Cars/src/main/java/com/pluralsight/Main.java
+++ Cars/src/main/java/com/pluralsight/Main.java
@@ -3,8 +3,10 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args){
 
+        Vehicle vehicle = new Vehicle(1234567, "Yamaha R6", "Black", 37000,7500);
+
+        System.out.println(vehicle);
+
         
-
-
     }
 }
